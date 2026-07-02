e3d3f0db26db838835854a7383e7cd08c9c96beb
teadhch <teadhch@naver.com>
2026-07-02 11:34:28 +0900

    feat : props로 넘겨온 onCreate를 이용하여 유저가 할일을 입력하고 추가 버튼을 클릭 했을때 할일이 todo(state)에 저장되도록

--- src/components/TodoEditor.jsx
+++ src/components/TodoEditor.jsx
@@ -1,12 +1,24 @@
 import "./TodoEditor.css";
+import { useState } from "react";
 
-const TodoEditor = () => {
+const TodoEditor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  const onSubmit = () => {
+    onCreate(content);
+  };
   return (
     <div className="TodoEditor">
       <h4>새로운 할일 작성하기</h4>
       <div className="editor_wrapper">
-        <input placeholder="새로운 Todo..." />
-        <button>추가</button>
+        <input
+          placeholder="새로운 Todo..."
+          value={content}
+          onChange={onChangeContent}
+        />
+        <button onClick={onSubmit}>추가</button>
       </div>
     </div>
   );
