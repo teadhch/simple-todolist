bf00606fc89a8f16f2aa19d444cd0ed99e3c51d7
teadhch <teadhch@naver.com>
2026-07-02 09:40:26 +0900

    feat : TodoEditor 컴포넌트 제작 + 배치

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import TodoEditor from "./components/TodoEditor";
 
 function App() {
   return (
     <>
       <div className="App">
         <Header />
+        <TodoEditor />
         <div>Todo Editor</div>
         <div>Todo List</div>
       </div>
