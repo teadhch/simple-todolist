33d1d30e8bc696c6d3713776c4cf8c2282dc5c28
teadhch <teadhch@naver.com>
2026-07-02 11:42:49 +0900

    feat : todoEditor 개선 (할일 내용이 없으면 저장되지 않도록, 엔터키를 눌러도 저장이 될 수 있도록)

--- src/components/TodoEditor.jsx
+++ src/components/TodoEditor.jsx
@@ -1,22 +1,37 @@
 import "./TodoEditor.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const TodoEditor = ({ onCreate }) => {
   const [content, setContent] = useState("");
+  const inputRef = useRef(null);
   const onChangeContent = (e) => {
     setContent(e.target.value);
   };
   const onSubmit = () => {
+    if (!content) {
+      alert("할일을 입력하세요!");
+      inputRef.current.focus();
+      return;
+    }
     onCreate(content);
+    setContent("");
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode == 13) {
+      onSubmit();
+    }
   };
   return (
     <div className="TodoEditor">
       <h4>새로운 할일 작성하기</h4>
       <div className="editor_wrapper">
         <input
+          ref={inputRef}
           placeholder="새로운 Todo..."
           value={content}
           onChange={onChangeContent}
+          onKeyDown={onKeyDown}
         />
         <button onClick={onSubmit}>추가</button>
       </div>
