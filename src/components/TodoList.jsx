cd2ce71741427af18a87d083fad3e717ff30dddc
teadhch <teadhch@naver.com>
2026-07-02 14:01:34 +0900

    feat : TodoList.jsx 저장

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = ({ todo }) => {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <div className="TodoList">
       <h4>Todo List</h4>
-      <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <input
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+        value={search}
+        onChange={onChangeSearch}
+      />
       <div className="list_wrapper">
         {todo.map((it) => (
           <TodoItem key={it.id} {...it} />
