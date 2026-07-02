be6922001b2ba748e33082ba193181867b8809ce
teadhch <teadhch@naver.com>
2026-07-02 11:01:47 +0900

    feat : todo스테이트 생성.mockTodo(목업데이터) 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
 import TodoList from "./components/TodoList";
 
+const mockTodo = [
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "청소하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 3,
+    isDone: false,
+    content: "게으름피기",
+    createdDate: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todo, setTodo] = useState(mockTodo);
   return (
     <>
       <div className="App">
         <Header />
         <TodoEditor />
-        <TodoList />
+        <TodoList todo={todo} />
       </div>
     </>
   );
