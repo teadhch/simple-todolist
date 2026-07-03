75f7459c95e711b968e47548d59cd770a1f2c40a
teadhch <teadhch@naver.com>
2026-07-03 09:33:02 +0900

    feat : 할일 검색어 기능 추가 (추가된 내용은 주석으로 설명함)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,6 +7,19 @@ const TodoList = ({ todo }) => {
   const onChangeSearch = (e) => {
     setSearch(e.target.value);
   };
+
+  const getSearchRsult = () => {
+    // 검색어(search)가 없으면 todo(할일 전체목록)를 반환하고,
+    // 검색어가 있으면 할일목록(배열)을 순회하여, 검색어가 할일 내용중에 포함된 할일들만 추려서
+    // 새로운 배열에 넣고 그 주소를 반환한다.
+    // 할일의 내용과 검색어를 모두 소문자변환하여 영어 대소문자를 구분하지 않고 검색 되도록...
+    return search === ""
+      ? todo
+      : todo.filter((it) =>
+          it.content.toLowerCase().includes(search.toLowerCase()),
+        );
+  };
+
   return (
     <div className="TodoList">
       <h4>Todo List</h4>
@@ -17,7 +30,8 @@ const TodoList = ({ todo }) => {
         onChange={onChangeSearch}
       />
       <div className="list_wrapper">
-        {todo.map((it) => (
+        {/* getSearchResult() 함수가 호출된 결과 또한 배열이므로, map을 사용할 수 있고, filter()를 사용했기 때문에 호출 될 때마다 매번 새로운 배열의 주소값으로 교체되어 아래의 부분이 재 렌더링 된다. 또한 filter()가 얕은 복사를 수행하기 때문에 기존의 할일목록 (todo)는 수정되지 않는다 */}
+        {getSearchRsult().map((it) => (
           <TodoItem key={it.id} {...it} />
         ))}
       </div>
