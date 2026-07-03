dda1ea05c1fde671d730e68b5dacb7f4d73c4c6d
teadhch <teadhch@naver.com>
2026-07-03 10:09:43 +0900

    feat : 수정될 할일의 번호(targetId)를 받아 해당하는 번호의 할일을 찾고, 수정된 할일의 isDone 속성을 반전(not 연산자) 시키는 onUpdate()를 만들었고, props로 내려줌

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,8 @@ const mockTodo = [
 function App() {
   const [todo, setTodo] = useState(mockTodo);
   const idRef = useRef(4);
+
+  // 새로운 할일 추가
   const onCreate = (content) => {
     const newItem = {
       id: idRef.current,
@@ -39,12 +41,26 @@ function App() {
     setTodo([newItem, ...todo]); // 기존의 todo 앞에 새로운 할일 객체 추가 후 새 배열반환
     idRef.current += 1; // 할일 객체의 id값 1 증가 (id값이 중복 되지 않도록)
   };
+
+  // 할일 수정하기
+  const onUpdate = (targetId) => {
+    setTodo(
+      todo.map((it) => {
+        if (it.id === targetId) {
+          // 수정할 할일을 찾음
+          return { ...it, isDone: !it.isDone }; //수정해서 반환해주면 map이 새로운 배열을 setTodo로 바꾸고
+        } else {
+          return it;
+        }
+      }),
+    );
+  };
   return (
     <>
       <div className="App">
         <Header />
         <TodoEditor onCreate={onCreate} /> {/* 함수도 props로 내려줄 수 있다 */}
-        <TodoList todo={todo} />
+        <TodoList todo={todo} onUpdate={onUpdate} />
       </div>
     </>
   );
